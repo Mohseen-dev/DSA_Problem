79dca9704bf3c5c7ff8fdfd8217fb0342a4d9431
Mohseen-dev <m9084097960@gmail.com>
2026-07-23 00:24:00 +0530

    Time: 2 ms (99.29%) | Memory: 44 MB (96.55%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,28 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       if(s.equals(" ")){
-        return true;
-       }
-       int first = 0;
-       int last = s.length()-1;
-       while(first <= last){
-        char currfirst = s.charAt(first);
-        char currlast = s.charAt(last);
-        if(!Character.isLetterOrDigit(currfirst)){
-            first++;
-        }
-        else if(!Character.isLetterOrDigit(currlast)){
-            last--;
-        }else {
-            if(Character.toLowerCase(currfirst) != Character.toLowerCase(currlast)){
-                return false;
+        int start = 0;
+        int last = s.length() - 1;
+
+        while (start <= last) {
+            char startCurr = s.charAt(start);
+            char lastCurr = s.charAt(last);
+
+            if (!((startCurr >= 97 && startCurr <= 122) || (startCurr >= 65 && startCurr <= 90)
+                    || (startCurr >= 48 && startCurr <= 57))) {
+                start++;
+            } else if (!((lastCurr >= 97 && lastCurr <= 122) || (lastCurr >= 65 && lastCurr <= 90)
+                    || (lastCurr >= 48 && lastCurr <= 57))) {
+                last--;
+            } else {
+                if (Character.toLowerCase(startCurr) == Character.toLowerCase(lastCurr)) {
+                    start++;
+                    last--;
+                } else {
+                    return false;
+                }
             }
-            first++;
-            last--;
         }
-
-       }
         return true;
-
     }
 }
